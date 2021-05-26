93dee46317f36c57efea98bb4e282243d72c4dc3
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-26 23:49:58 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/RandomData.pm
@@ -0,0 +1,40 @@
+package Acme::CPANModules::RandomData;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use strict;
+use Acme::CPANModulesUtil::Misc;
+
+my $text = <<'_';
+**Generating**
+
+<pm:Data::Random> generates random number, random generated word, random
+dictionary word (default wordlist provided), date (in YYYY-MM-DD format), time
+(in HH::MM:SS format), datetime, image (just a blank PNG with random size and
+background color).
+
+For more specific types of random data (person, password, etc), see other lists
+mentioned below.
+
+Keywords: random data.
+
+_
+
+our $LIST = {
+    summary => 'Generating random person (name, title, age, etc)',
+    description => $text,
+};
+
+Acme::CPANModulesUtil::Misc::populate_entries_from_module_links_in_description;
+
+1;
+# ABSTRACT:
+
+=head1 prepend:SEE ALSO
+
+L<Acme::CPANModules::RandomPerson>
+
+L<Acme::CPANModules::RandomPassword>
